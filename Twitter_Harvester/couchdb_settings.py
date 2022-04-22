00ec5607d8486ca1b5a8c242eb58e240582d83de
Natsu <weimin.ouyang@gmail.com>
2022-04-22 11:43:17 +1000

    updated couchdb settings

--- Twitter_Harvester/couchdb_settings.py
+++ Twitter_Harvester/couchdb_settings.py
@@ -1,9 +1,8 @@
 # couchbd_settings
 # some settings may need to be changed when connecting to MRC
 
-address = 'localhost:5984' 
+address = '172.26.130.201:5984' 
 username = 'grp5admin'
 password = 'password'
 tweets = 'raw_tweets'
 user = 'user_list'
-topic = 'topic-text'
