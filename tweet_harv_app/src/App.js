61921c65abed34fa4287c7e7bbdf32c2e0e1fa2a
yulingz3 <yulingz3@student.unimelb.edu.au>
2022-04-28 11:37:12 +1000

    Frontend get data draft
    Frontend get data draft

--- tweet_harv_app/src/App.js
+++ tweet_harv_app/src/App.js
@@ -5,8 +5,24 @@ import MapView from './components/map/MapView';
 import Home from './components/homepage/Home';
 import Sidebar from './components/sidebar/Sidebar';
 import Chart from './components/charts/Chart';
+import React, { useEffect } from 'react';
 
 function App() {
+  // const axios = require("axios").default; 
+
+  // useEffect(() => {
+  //   // axios.get("http://localhost:8000/map")
+  //   axios.get("http://grp5admin:password@172.26.134.124:5984/raw_tweets/_design/geo/_view/coordinates-count-1")
+  //   .then(function (response){
+  //     console.log(response.data);   
+  //   })
+  //   .catch(function (error){
+  //     console.log(error); 
+  //   })
+  //   .then(function(){
+
+  //   }); 
+  // }); 
   return (
     <div  className="App">
       <Router>
