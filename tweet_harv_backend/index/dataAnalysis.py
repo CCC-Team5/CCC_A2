467ada373036ecbe8055776496b64503f4004081
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-04-30 21:02:09 +0800

    Update dataAnalysis.py

--- tweet_harv_backend/index/dataAnalysis.py
+++ tweet_harv_backend/index/dataAnalysis.py
@@ -100,3 +100,53 @@ def top_n_lang_count(db, langCode_path, N):
     languages = {v2: v1 for k1, v1 in languages.items() for k2, v2 in langCode.items() if k1 == k2}
 
     return languages
+
+def top_n_birth_country(file_path, N):
+    """
+    extract top N non-English-speaking countries where people living in the Greater Melbourne were originally from
+    params: path to census data download from AURIN - 'country_of_birth.csv';
+            number of non-English-speaking countries to extract
+    return: top N non-English-speaking countries' names, total population count, and percentage population
+    return type: numpy arrays
+    frontend: bar chart/pie chart (colour matching for most tweeted languages/counrty of birth/language spoken at home)
+    """
+    data = pd.read_csv(file_path)
+    
+    match_cols = []
+    new_cols = []
+    col_names = data.columns
+    for name in col_names:
+        if name.endswith('_p'):
+            match_cols.append(name)
+            new_cols.append(name.strip())
+
+    ext_data = pd.DataFrame(data[match_cols])
+    ext_data.columns = new_cols
+    
+    grand_total = ext_data['tot_p'].sum(axis = 0)
+    country_total = ext_data.sum(axis = 0)
+    percentage = country_total/grand_total * 100
+    
+    birth_country = pd.DataFrame(country_total, columns = ['country_total'])
+    birth_country['percentage'] = percentage
+    
+    drop_columns = ['hong_kong_sar_china_p', 'born_elsewhere_p', 'tot_p', 'os_visitors_p', 'country_birth_not_stated_p', 'australia_p', 'new_zealand_p', 'united_states_america_p', 'united_kingdom_ci_im_p', 'fiji_p', 'south_africa_p']
+    
+    birth_country.loc['china_excl_sars_taiwan_p'] += birth_country.loc['hong_kong_sar_china_p']
+    birth_country = birth_country.T.drop(drop_columns, axis = 1)
+    birth_country = birth_country.rename({'china_excl_sars_taiwan_p' : 'china_p', 'sri_lanka_p' : 'srilanka_p'}, axis = 1)
+    
+    country_names = []
+    for item in birth_country.columns:
+        item = item.split('_')
+        country_names.append(item[0].capitalize())
+
+    birth_country.columns = country_names
+    birth_country = birth_country.rename({'Srilanka' : 'Sri Lanka'}, axis = 1).T
+    birth_country = birth_country.sort_values(by = ['country_total'], ascending = False)[:N]
+
+    birth = {}
+    for i in range(len(birth_country)):
+        birth[birth_country.index[i]] = birth_country.country_total.values[i], birth_country.percentage.values[i]
+    
+    return birth
