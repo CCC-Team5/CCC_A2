2e5f8a0b66935d9f997ad836c253b42db8724417
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-04-30 21:03:35 +0800

    Update urls.py

--- tweet_harv_backend/index/urls.py
+++ tweet_harv_backend/index/urls.py
@@ -6,4 +6,5 @@ app_name = 'index'
 urlpatterns = [
     path("hashtag/", views.hashtag),
     path("language_at_home/", views.lang_spoken_home),
+    path("hashtag_top_n_birth_country", views.hashtag_top_n_birth_country),
 ]
