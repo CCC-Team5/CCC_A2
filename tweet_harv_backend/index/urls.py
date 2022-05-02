559b1c77535a53eb54a76bf49fff203801877510
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-05-02 16:26:42 +0800

    Update urls.py

--- tweet_harv_backend/index/urls.py
+++ tweet_harv_backend/index/urls.py
@@ -6,5 +6,5 @@ app_name = 'index'
 urlpatterns = [
     path("hashtag/", views.hashtag),
     path("opportunity/", views.language_and_birth),
-
+    path("housing/trend_sentiment/", views.housing_trend_sentiment),
 ]
