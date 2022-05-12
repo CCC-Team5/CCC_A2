ef8b33ec13d5327487a2e8f1ae6bd77d10c56365
xhhh-mendes <kirikirimendes@gmail.com>
2022-05-12 10:46:42 +1000

    Minor on CORS

--- tweet_harv_app/src/setupProxy.js
+++ tweet_harv_app/src/setupProxy.js
@@ -1,13 +1,13 @@
-// const {createProxyMiddleware} = require('http-proxy-middleware')
+const {createProxyMiddleware} = require('http-proxy-middleware')
 
-// module.exports = function (app) {
-//     // '/api': replace 'target' to /api
-//     // target: url want to replace
-//     // changeOrigin: change header
-//     app.use(createProxyMiddleware('/api', {
-//         target: 'http://localhost:8000',
-//         secure: false,
-//         changeOrigin: true,
-//         pathRewrite: { '^/api': '' } //去除请求前缀，保证交给后台服务器的是正常请求地址（必须配置
-//     }))
-// }
+module.exports = function (app) {
+    // '/api': replace 'target' to /api
+    // target: url want to replace
+    // changeOrigin: change header
+    app.use(createProxyMiddleware('/api', {
+        target: 'http://localhost:8000',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' } //remove to ensure the address is normal
+    }))
+}
