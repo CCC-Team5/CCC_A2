c81995c22718f6d10a10124bb3430452697a72e1
yulingz3 <yulingz3@student.unimelb.edu.au>
2022-05-14 15:20:42 +1000

    new markers added
    new markers added

--- tweet_harv_app/src/components/map/MapView.jsx
+++ tweet_harv_app/src/components/map/MapView.jsx
@@ -18,6 +18,16 @@ import {
   Popup,
 } from 'react-map-gl';
 import Pin from './pin.tsx';
+import Pin2 from './pin2.tsx';
+import { MdAccountBalance } from 'react-icons/md';
+import {MdDirectionsTransitFilled} from 'react-icons/md';
+import {MdFlight} from 'react-icons/md';
+import {MdOutlineSchool} from 'react-icons/md'; 
+import {MdShoppingCart} from 'react-icons/md'; 
+import {ImBooks} from 'react-icons/im'; 
+import {GiChurch} from 'react-icons/gi';
+import {FaGolfBall} from 'react-icons/fa'; 
+import {FaUmbrellaBeach} from 'react-icons/fa'; 
 
 const ZOOM_LEVELES = {
 	'heatmap': 8,
@@ -61,17 +71,22 @@ const MapView = () => {
   }, [])
 
   const marker_data = [
-    { "city": "Melbourne Central", "image": "https://www.shoppingcentrenews.com.au/wp-content/uploads/2022/02/Melbourne-Central-5.jpg", "state": "Melbourne", "latitude": -37.81004, "longitude": 144.96277 },
-    { "city": "University of Melbourne", "image": "https://www.unimelb.edu.au/__data/assets/image/0016/4004026/17099_0019.jpg", "state": "Melbourne", "latitude": -37.79818, "longitude": 144.96094 },
-    { "city": "State Library Victoria", "image": "https://www.slv.vic.gov.au/sites/default/files/styles/feature_image/public/strategic-plan-hero.jpg?itok=CCmv7jIG", "state": "Melbourne", "latitude": -37.80964, "longitude": 144.96520 },
-    { "city": "Melbourne Airport", "image": "https://grimshaw.global/assets/lib/2021/05/21/210409_Grimshaw_Airport-0242.jpg", "state": "Melbourne", "latitude": -37.67065, "longitude": 144.84328 }, 
-    { "city": "Federation Square", "image": "https://images.squarespace-cdn.com/content/v1/5e57b89fbb687c386d2d6be8/1605283435752-D1BL0DKTGZ855OT42T77/Federation-Square-site-02.jpg", "state": "Melbourne", "latitude": -37.81791, "longitude": 144.96907 }, 
-    { "city": "St Paul's Cathedral", "image": "http://shipoffools.com/wp-content/uploads/2018/02/melbourne_paul-1.jpg", "state": "Melbourne", "latitude": -37.81688, "longitude": 144.96765 }, 
-    { "city": "Southern Cross", "image": "https://www.railway-technology.com/wp-content/uploads/sites/13/2017/10/1-image-11.jpg", "state": "Melbourne", "latitude": -37.81824, "longitude": 144.95254 }, 
-    { "city": "Marvel Stadium", "image": "https://www.delawarenorth.com/~/media/delawarenorth/images/venue%20images/australia/marvel-stadium-t1.jpg?h=350&la=en&w=804", "state": "Melbourne", "latitude": -37.81638, "longitude": 144.94752 }, 
-    { "city": "Queens Victoria Market", "image": "http://syn.org.au/app/uploads/Screen-Shot-2018-05-08-at-9.13.58-am-768x432.png", "state": "Melbourne", "latitude": -37.80733, "longitude": 144.95676 }, 
-    { "city": "Parliament House", "image": "https://i.pinimg.com/originals/d4/66/17/d46617a8f5b5783d09b4d2d39a2817b5.jpg", "state": "Melbourne", "latitude": -37.81067, "longitude": 144.97384 },
-    { "city": "South Melbourne Market", "image": "https://images.squarespace-cdn.com/content/v1/58d860ab29687f4e2b16bbb9/1580794640363-3FQKU7A9M2ENK575GO3J/IMG_4432-1.jpg", "state": "Melbourne", "latitude": -37.83119, "longitude": 144.95580 }];
+    { "city": "Melbourne Central", "icon": "shopping", "image": "https://www.shoppingcentrenews.com.au/wp-content/uploads/2022/02/Melbourne-Central-5.jpg", "state": "Melbourne", "latitude": -37.81004, "longitude": 144.96277 },
+    { "city": "University of Melbourne", "icon": "university", "image": "https://www.unimelb.edu.au/__data/assets/image/0016/4004026/17099_0019.jpg", "state": "Melbourne", "latitude": -37.79818, "longitude": 144.96094 },
+    { "city": "State Library Victoria", "icon": "library", "image": "https://www.slv.vic.gov.au/sites/default/files/styles/feature_image/public/strategic-plan-hero.jpg?itok=CCmv7jIG", "state": "Melbourne", "latitude": -37.80964, "longitude": 144.96520 },
+    { "city": "Melbourne Airport", "icon": "airport", "image": "https://grimshaw.global/assets/lib/2021/05/21/210409_Grimshaw_Airport-0242.jpg", "state": "Melbourne", "latitude": -37.67065, "longitude": 144.84328 }, 
+    { "city": "Federation Square", "icon": "building", "image": "https://images.squarespace-cdn.com/content/v1/5e57b89fbb687c386d2d6be8/1605283435752-D1BL0DKTGZ855OT42T77/Federation-Square-site-02.jpg", "state": "Melbourne", "latitude": -37.81791, "longitude": 144.96907 }, 
+    { "city": "St Paul's Cathedral","icon": "church",  "image": "http://shipoffools.com/wp-content/uploads/2018/02/melbourne_paul-1.jpg", "state": "Melbourne", "latitude": -37.81688, "longitude": 144.96765 }, 
+    { "city": "Southern Cross", "icon": "trainStation", "image": "https://www.railway-technology.com/wp-content/uploads/sites/13/2017/10/1-image-11.jpg", "state": "Melbourne", "latitude": -37.81824, "longitude": 144.95254 }, 
+    { "city": "Marvel Stadium", "icon": "building", "image": "https://www.delawarenorth.com/~/media/delawarenorth/images/venue%20images/australia/marvel-stadium-t1.jpg?h=350&la=en&w=804", "state": "Melbourne", "latitude": -37.81638, "longitude": 144.94752 }, 
+    { "city": "Queens Victoria Market","icon": "shopping",  "image": "http://syn.org.au/app/uploads/Screen-Shot-2018-05-08-at-9.13.58-am-768x432.png", "state": "Melbourne", "latitude": -37.80733, "longitude": 144.95676 }, 
+    { "city": "Parliament House","icon": "building",  "image": "https://i.pinimg.com/originals/d4/66/17/d46617a8f5b5783d09b4d2d39a2817b5.jpg", "state": "Melbourne", "latitude": -37.81067, "longitude": 144.97384 },
+    { "city": "South Melbourne Market","icon": "shopping",  "image": "https://images.squarespace-cdn.com/content/v1/58d860ab29687f4e2b16bbb9/1580794640363-3FQKU7A9M2ENK575GO3J/IMG_4432-1.jpg", "state": "Melbourne", "latitude": -37.83119, "longitude": 144.95580 }, 
+    { "city": "Ss Peter & Paul Ukrainian Catholic Cathedral","icon": "church",  "image": "https://auv.org.au/wp-content/uploads/2021/04/katedra11.jpg", "state": "Melbourne", "latitude": -37.79600, "longitude": 144.94390 }, 
+    { "city": "Sunshine Golf Club","icon": "golf",  "image": "https://golf-designers.com/wp-content/uploads/2012/09/MD-PCD-2.jpg", "state": "Melbourne", "latitude": -37.79469, "longitude": 144.77586 }, 
+    { "city": "Chelsea Beach","icon": "beach",  "image": "https://lh5.googleusercontent.com/p/AF1QipOR4EVjhOEqq_6uedpD0d1KiZKXYhX2Kyz1jioQ=w500-h500-k-no", "state": "Melbourne", "latitude": -38.05285, "longitude": 145.11363 }, 
+    { "city": "St Kilda Beach","icon": "beach",  "image": "https://exp.cdn-hotels.com/hotels/44000000/43010000/43007200/43007149/eefca7bf_z.jpg?impolicy=fcrop&w=500&h=333&q=medium", "state": "Melbourne", "latitude": -37.86771, "longitude": 144.97397 }, 
+    { "city": "Chapel Street","icon": "shopping",  "image": "https://media.timeout.com/images/105739823/image.jpg", "state": "Melbourne", "latitude": -37.83748, "longitude": 144.99593 }];
 
   const pins = useMemo(
     () =>
@@ -88,7 +103,16 @@ const MapView = () => {
             setPopupInfo(city);
           }}
         >
-          <Pin />
+          {city.icon === 'building' && <MdAccountBalance color= "white"/>} 
+          {city.icon === 'trainStation' && <MdDirectionsTransitFilled color= "white" />}  
+          {city.icon === 'airport' && <MdFlight color= "white"/>} 
+          {city.icon === 'university' && <MdOutlineSchool color= "white"/>}
+          {city.icon === 'shopping' && <MdShoppingCart color= "white"/>} 
+          {city.icon === 'library' && <ImBooks color= "white"/>} 
+          {city.icon === 'church' && <GiChurch color= "white"/>} 
+          {city.icon === 'golf' && <FaGolfBall color= "white"/>}
+          {city.icon === 'beach' && <FaUmbrellaBeach color= "white"/>}
+
           {/* <img src="https://materialdesignicons.com/api/download/icon/svg/379B9D93-434B-46E7-9ABC-CAFAB694B209" /> */}
         </Marker>
       )),
@@ -197,6 +221,7 @@ const MapView = () => {
         }}
       >
         <FormControl>
+          {/* <FormLabel id="demo-row-radio-buttons-group-label"><h4 style={{color:"black", fontFamily:"sans-serif"}}>Choose Map Layer: </h4></FormLabel> */}
           <FormLabel id="demo-row-radio-buttons-group-label"></FormLabel>
           <RadioGroup onChange={onRadioChange} value={selectedMapType}>
             <FormControlLabel value="heatmap" control={<Radio />} label="Heatmap" />
--- tweet_harv_app/src/components/map/circle-style.ts
+++ tweet_harv_app/src/components/map/circle-style.ts
@@ -9,33 +9,35 @@ export const circleLayer: CircleLayer = {
     'minzoom': 8,
     'paint': {
         // Size circle radius by earthquake magnitude and zoom level
-        'circle-radius': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            7,
-            ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
-            16,
-            ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
-        ],
+        'circle-radius': 10,
+        // 'circle-radius': [
+        //     'interpolate',
+        //     ['linear'],
+        //     ['zoom'],
+        //     7,
+        //     ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+        //     16,
+        //     ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+        // ],
         // Color circle by earthquake magnitude
-        'circle-color': [
-            'interpolate',
-            ['linear'],
-            ['get', 'mag'],
-            20,
-            'rgba(255,0,0,0)',
-            200,
-            'rgb(255,51,51)',
-            300,
-            'rgb(255,102,102)',
-            400,
-            'rgb(253,219,199)',
-            500,
-            'rgb(255,204,255)',
-            1000, 
-            'rgb(255,204,255)'
-        ],
+        'circle-color': '#007cbf', 
+        // 'circle-color': [
+        //     'interpolate',
+        //     ['linear'],
+        //     ['get', 'mag'],
+        //     20,
+        //     'rgba(255,0,0,0)',
+        //     200,
+        //     'rgb(255,51,51)',
+        //     300,
+        //     'rgb(255,102,102)',
+        //     400,
+        //     'rgb(253,219,199)',
+        //     500,
+        //     'rgb(255,204,255)',
+        //     1000, 
+        //     'rgb(255,204,255)'
+        // ],
         'circle-stroke-color': 'white',
         'circle-stroke-width': 0.9,
         // Transition from heatmap to circle layer by zoom level
