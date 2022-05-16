f353bd2f14d82fab78d0b400fff2a405ad29844c
xhhh-mendes <kirikirimendes@gmail.com>
2022-05-16 21:23:44 +1000

    Minor Change on deployment echo

--- deployment/exec-grp5-deploy.sh
+++ deployment/exec-grp5-deploy.sh
@@ -9,30 +9,30 @@ sudo chmod 600 grp5-dependencies/grp5_key.pem
 sudo chmod 600 grp5-couchdb-nodes-cluster/grp5_key.pem
 sudo chmod 600 grp5-deploy_app/grp5_key.pem
 
-echo "##### EXEC ANSIBLE build base infrastructure #####"
+echo "#### Execute first Ansible module to build base infrastructure ####"
 cd grp5-base_infrastructure
 sh run-base-deploy.sh
 
-echo "##### Moving hosts from Infra to Dependencies#####"
+echo "#### Moving hosts from First module to the Second ####"
 cd ..
 cp -rf grp5-base_infrastructure/hosts grp5-dependencies
 cp -rf grp5-base_infrastructure/hosts grp5-deploy_app
 
-echo "##### EXEC ANSIBLE DEPENDENCIES #####"
+echo "#### Execute second Ansible module to install dependencies ####"
 cd grp5-dependencies
 sh run-dependencies.sh
 
-echo "##### Moving files from Infra to couch nodes cluster deployment #####"
+echo "#### Moving files from First module to the Third ####"
 cd ..
 cp -rf grp5-base_infrastructure/inventory/hosts_couch grp5-couchdb-nodes-cluster
 cp -rf grp5-base_infrastructure/inventory/g5-couch-dynamic-vars.yaml grp5-couchdb-nodes-cluster/host_vars
 rm -r grp5-base_infrastructure/inventory/g5-couch-dynamic-vars.yaml
 
-echo "#### DEPLOY COUCH MULTI-NODE CLUSTER ####"
+echo "#### Execute third Ansible module to add nodes into CouchDB Cluster ####"
 cd grp5-couchdb-nodes-cluster
 sh run-couch-cluster.sh
 
-echo "#### DEPLOY & RUN APPLICATION ####"
+echo "#### Execute last Ansible module to deploy and run Twitter harvester & Web application ####"
 cd ..
 cd grp5-deploy_app
 sh run-deploy-application.sh
