d42cf2b9789ef5beabedd70dfa9af476ae83afe1
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-05-16 09:30:35 +0800

    Update env.js

--- tweet_harv_app/src/env.js
+++ tweet_harv_app/src/env.js
@@ -1,2 +1,12 @@
+// Part of Assignment 2 - Cluster and Cloud Computing (COMP90024) Group 5
+
+// Author:
+
+// Xinhao Chen 1230696 Melbourne
+// Weimin Ouyang 340438 Melbourne
+// Tianqi Yu 1221167 China
+// Junjie Xia 1045673 China
+// Yuling Zheng 954408 Melbourne
+
 export const MAPBOX_TOKEN = 'sk.eyJ1IjoieGhoaC1tZW5kZXMiLCJhIjoiY2wyYTZ1amMxMDJpczNqcWl1cDlzeWtnMiJ9.3CWkHVVtiOcyJn3VqvS7oA'
 export const MAPBOX_PT = "pk.eyJ1IjoieGhoaC1tZW5kZXMiLCJhIjoiY2wyYTZxemlpMDJnOTNkbGgxd285ZWptcCJ9.aA0z5X2h4XABFswlLe-Y_A"
