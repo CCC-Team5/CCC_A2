5b944a95ce72cd3fe9d472eff006ea73f8bd8a1e
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-05-16 09:34:16 +0800

    Update dataAnalysis.py

--- tweet_harv_backend/index/dataAnalysis.py
+++ tweet_harv_backend/index/dataAnalysis.py
@@ -1,7 +1,13 @@
 """
-Author
-Weimin Ouyang 340438 completing all data analysis section before 2022/4/22
-yutianqi 1221167 packaging all functions
+Part of Assignment 2 - Cluster and Cloud Computing (COMP90024) Group 5
+
+Author:
+
+Xinhao Chen 1230696 Melbourne
+Weimin Ouyang 340438 Melbourne
+Tianqi Yu 1221167 China
+Junjie Xia 1045673 China
+Yuling Zheng 954408 Melbourne
 
 Description: data analysis in couchdb database
 """
