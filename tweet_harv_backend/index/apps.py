7269e6e6065cc598b302b477c8b1d5fe2e50ed3b
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-05-16 09:33:38 +0800

    Update apps.py

--- tweet_harv_backend/index/apps.py
+++ tweet_harv_backend/index/apps.py
@@ -1,3 +1,13 @@
+# Part of Assignment 2 - Cluster and Cloud Computing (COMP90024) Group 5
+
+# Author:
+
+# Xinhao Chen 1230696 Melbourne
+# Weimin Ouyang 340438 Melbourne
+# Tianqi Yu 1221167 China
+# Junjie Xia 1045673 China
+# Yuling Zheng 954408 Melbourne
+
 from django.apps import AppConfig
 
 
