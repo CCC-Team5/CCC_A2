343d10643e092e192149a1aaa2d687faf479ddb3
StevenNo1 <73212528+StevenNo1@users.noreply.github.com>
2022-05-16 09:34:35 +0800

    Update database.py

--- tweet_harv_backend/index/database.py
+++ tweet_harv_backend/index/database.py
@@ -1,7 +1,13 @@
 """
-Author
-Weimin Ouyang 340438
-yutianqi 1221167
+Part of Assignment 2 - Cluster and Cloud Computing (COMP90024) Group 5
+
+Author:
+
+Xinhao Chen 1230696 Melbourne
+Weimin Ouyang 340438 Melbourne
+Tianqi Yu 1221167 China
+Junjie Xia 1045673 China
+Yuling Zheng 954408 Melbourne
 
 2022/4/24
 Description: basic operations on couchdb, like connect, create
